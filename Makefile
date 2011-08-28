09bc23eba203ab1754e6de6f46734b6530d4cd4d
roughl <r0ugh_l@yahoo.de>
2011-08-28 11:42:50 +0200

    Add --digest option to initialize srand
    This option lets you choose a digest algorithm which will be used to
    calculate the message digest of all passed arguments. The first four
    bytes of the digest will then be used to initialize srand() instead
    of the current time

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 GCC = gcc
+LIBFLAGS = `pkg-config --libs openssl`
 CFLAGS = -Wall -g -O2 -pipe -std=c99
 
 all: decision
 
 decision: decision.c
-	${GCC} ${CFLAGS} -o decision decision.c
+	${GCC} ${CFLAGS} ${LIBFLAGS} -o decision decision.c
--- decision.c
+++ decision.c
@@ -1,20 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <openssl/evp.h>
 
 void die()
 {
-	fprintf(stderr, "usage: decision arg1 [arg2 .. argn]\n");
+	fprintf(stderr, "usage: decision [--digest md4|md5|sha|ripemd] arg1 [arg2 .. argn]\n");
 	exit(1);
 }
 
 int main(int argc, char* argv[])
 {
-	if(argc<2)
-	{
+	if(argc<2) {
 		die();
 	}
-	int random = time(0) % (argc-1)+1;
+	srand(time(0));
+	int base = 1;
+	if(strcmp(argv[1], "--digest") == 0) {
+		if(argc<4) {
+			die();
+		}
+		base+=2;
+		EVP_MD_CTX mdctx;
+		const EVP_MD *md;
+		unsigned char md_value[EVP_MAX_MD_SIZE];
+		unsigned int md_len;
+		OpenSSL_add_all_digests();
+
+		md = EVP_get_digestbyname(argv[2]);
+		if( !md ) {
+			fprintf(stderr, "digest not recognised\n");
+			die();
+		}
+
+		EVP_MD_CTX_init(&mdctx);
+		EVP_DigestInit_ex(&mdctx, md, NULL);
+		for(unsigned i=3; i<argc; i++) {
+			EVP_DigestUpdate(&mdctx, argv[i], strlen(argv[i]));
+		}
+		EVP_DigestFinal_ex(&mdctx, md_value, &md_len);
+		EVP_MD_CTX_cleanup(&mdctx);
+		unsigned int init = *((unsigned int*)md_value);
+		srand(init);
+	}
+	
+	int random = rand() % (argc-base)+base;
 
 	printf("%s\n", argv[random]);
 
